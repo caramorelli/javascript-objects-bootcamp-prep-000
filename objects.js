9e1f49ed6acb4cef91be77b8bc80b3c0b90af934
Cara Morelli <caramorelli7@gmail.com>
2018-08-20 00:14:13 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,6 +2,7 @@ var playlist = { song: 'Song Name'};
 
 function updatePlaylist(playlist, artist, song) {
   playlist[artist] = song;
+  return playlist;
 }
 
 function removeFromPlaylist(playlist, artist) {
